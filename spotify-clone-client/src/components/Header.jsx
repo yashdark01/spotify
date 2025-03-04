b33e523fbe270576df09c7914c4f240179ea33b4
yashdark01 <137425534+yashdark01@users.noreply.github.com>
2025-03-04 10:30:45 +0530

    spotify-clone-client

--- spotify-clone-client/src/components/Header.jsx
+++ spotify-clone-client/src/components/Header.jsx
@@ -1,3 +1,9 @@
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 import { FaHome } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
 import { BsBoxArrowDown } from "react-icons/bs";
@@ -6,11 +12,7 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between min-w-full w-screen  bg-black px-6 py-2  text-white">
       <div className="flex items-center gap-4">
-        <img
-          src="/spotify-icon.png"
-          alt="Spotify Logo"
-          className="w-8 h-8"
-        />
+        <img src="/spotify-icon.png" alt="Spotify Logo" className="w-8 h-8" />
         <FaHome className="text-xl text-gray-300" />
         <div className="relative flex items-center bg-[#121212] rounded-full px-4 py-1 w-96">
           <IoIosSearch className="text-gray-400 text-xl" />
@@ -28,10 +30,13 @@ const Header = () => {
         <span className="text-gray-500">|</span>
         <BsBoxArrowDown className="text-lg" />
         <span>Install App</span>
-        <span>Sign up</span>
-        <button className="bg-white text-black font-semibold px-4 py-1 rounded-full">
-          Log in
-        </button>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+
+        <SignedIn className="bg-black">
+          <UserButton />
+        </SignedIn>
       </div>
     </header>
   );
